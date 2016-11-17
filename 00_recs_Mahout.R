0e72d4a40e0f984c804741fc3056cd9c1574237c
Michael Gilbert <m.gilbert@outlook.com>
2016-11-17 16:15:25 -0500

    Updated 00_recs_Mahout and added output [MG]

--- 00_recs_Mahout.R
+++ 00_recs_Mahout.R
@@ -42,21 +42,25 @@ recs = recs[c(10, 1:9)]
 # Rename existing variables
 names(recs)[names(recs)=="overall"] = "overall.num"
 
-# Convert character features to numeric levels
+# Convert character features to factors
 recs$reviewerID = as.factor(recs$reviewerID)
 recs$asin = as.factor(recs$asin)
 
+# Create numeric versions of factors
+recs$reviewerID.num = recs$reviewerID
+recs$asin.num = recs$asin
+
 # Convert factor levels to numeric values
-levels(recs$reviewerID) = seq(from  = 1,
-                              to = nlevels(recs$reviewerID),
-                              by = 1)
-levels(recs$asin) = seq(from = 1,
-                        to = nlevels(recs$asin),
-                        by = 1)
+levels(recs$reviewerID.num) = seq(from  = 1,
+                                  to = nlevels(recs$reviewerID.num),
+                                  by = 1)
+levels(recs$asin.num) = seq(from = 1,
+                            to = nlevels(recs$asin.num),
+                            by = 1)
 
 # Convert factor features to numeric
-recs$reviewerID = as.numeric(as.character(recs$reviewerID))
-recs$asin = as.numeric(as.character(recs$asin))
+recs$reviewerID.num = as.numeric(as.character(recs$reviewerID.num))
+recs$asin.num = as.numeric(as.character(recs$asin.num))
 
 # Create flag for subset of recs
 recs$helpful.nan = sapply(recs$helpful,
@@ -66,22 +70,83 @@ recs$helpful.nan = sapply(recs$helpful,
 #------------------------------------------------------------------------------
 # Export
 #------------------------------------------------------------------------------
-
 # Subset recs for export
 recs = recs[recs$helpful.nan == 0, ]
 
-# Keep only three features
-recs = subset(recs, select = c(reviewerID,
-                               asin,
-                               overall.num))
-
 # Export for use in Mahout
-write.table(x = recs,
+write.table(x = recs[, c(11, 12, 7)],
             file = "recs.txt",
             quote = FALSE,
             sep = ",",
             col.names = FALSE,
             row.names = FALSE)
+
+#==============================================================================
+# Mahout Results
+#==============================================================================
+
+# Load recommendations
+temp = read.csv(file = "part-r-00000",
+                header = FALSE,
+                sep = "\t",
+                stringsAsFactors = FALSE)
+
+# Remove brackets
+temp$V2 = gsub(pattern = "\\[|\\]",
+               replacement = "",
+               x = temp$V2)
+
+# Reshape from semi-wide to long
+recs.out = data.frame()
+for (row in 1:nrow(temp)){
+    V1 = temp$V1[row]
+    V2 = unlist(strsplit(x = temp$V2[row],
+                         split = ",",
+                         fixed = TRUE))
+    recs.out = rbind(recs.out,
+                     cbind(rep(x = V1,
+                               times = length(V2)),
+                           V2))
+}
+
+# Only keep asin value (not score)
+recs.out$V2 = gsub(pattern = "^(.*?):.*",
+                   replacement = "\\1",
+                   x = recs.out$V2)
+
+# Change columns back to numeric for join
+recs.out$V1 = as.numeric(as.character(recs.out$V1))
+recs.out$V2 = as.numeric(as.character(recs.out$V2))
+
+# Rename columns
+colnames(recs.out) = c("reviewerID.num",
+                       "asin.num")
+
+# Clean up
+rm(temp); rm(row); rm(V1); rm(V2)
+
+#==============================================================================
+# Recommendations Dataset
+#==============================================================================
+
+# Replace numeric values with original values
+recs.out$reviewerID = recs$reviewerID[match(recs.out$reviewerID.num,
+                                            recs$reviewerID.num)]
+recs.out$asin = recs$asin[match(recs.out$asin.num,
+                                recs$asin.num)]
+
+# Drop numeric versions
+recs.out = recs.out[, 3:4]
+
+# Export
+write.table(x = recs.out,
+            file = "recs_out.csv",
+            quote = FALSE,
+            sep = ",",
+            row.names = FALSE)
+
+# Clean up
+rm(list = ls(pattern = "recs"))
 
 #==============================================================================
 # FIN
